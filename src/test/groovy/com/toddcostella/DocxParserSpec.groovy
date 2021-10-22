3bf7da2f5c2ee9bb3a3a33d2abbb7982c0d48ac7
Todd Costella <ToddCostella@gmail.com>
2021-10-22 19:35:46 +0000

    Initial commit

---
+++ src/test/groovy/com/toddcostella/DocxParserSpec.groovy
@@ -0,0 +1,19 @@
+package com.toddcostella
+
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import spock.lang.Specification
+import jakarta.inject.Inject
+
+@MicronautTest
+class DocxParserSpec extends Specification {
+
+    @Inject
+    EmbeddedApplication<?> application
+
+    void 'test it works'() {
+        expect:
+        application.running
+    }
+
+}
